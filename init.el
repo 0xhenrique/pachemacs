f8d7fcfcf2f7a6bdbec2ede4d313e4a5cb38d428
0xhenrique <allyson2308h@gmail.com>
2025-02-03 01:41:29 +0000

    wip: still needs to add ERC

--- init.el
+++ init.el
@@ -81,7 +81,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("51fa6edfd6c8a4defc2681e4c438caf24908854c12ea12a1fbfd4d055a9647a3" "d5fd482fcb0fe42e849caba275a01d4925e422963d1cd165565b31d3f4189c87" "8363207a952efb78e917230f5a4d3326b2916c63237c1f61d7e5fe07def8d378" default)))
+   '("0a2168af143fb09b67e4ea2a7cef857e8a7dad0ba3726b500c6a579775129635" "51fa6edfd6c8a4defc2681e4c438caf24908854c12ea12a1fbfd4d055a9647a3" "d5fd482fcb0fe42e849caba275a01d4925e422963d1cd165565b31d3f4189c87" "8363207a952efb78e917230f5a4d3326b2916c63237c1f61d7e5fe07def8d378" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
